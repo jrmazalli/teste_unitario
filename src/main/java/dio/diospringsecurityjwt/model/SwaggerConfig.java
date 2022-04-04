71fc3157c4fd039c16b34dfb556a1c7773c39b8e
Junior Mazalli <junior.mazalli@afgbrasil.com.br>
2022-04-04 11:58:32 -0300

    Habilitando segurança com Spring / h2 / JWT - Json web Token

---
+++ src/main/java/dio/diospringsecurityjwt/model/SwaggerConfig.java
@@ -0,0 +1,13 @@
+package dio.diospringsecurityjwt.model;
+
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+     
+     //...INSIRA OS CÓDIGOS ABAIXO
+
+}
---
+++ src/test/java/dio/diospringsecuriyjwt/DioSpringSecuriyJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.diospringsecuriyjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioSpringSecuriyJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
